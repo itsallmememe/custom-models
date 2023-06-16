b02439270b8f7f1d7c3de3bee96e1b4b19b63422
Niall McCormack <niall@itsallmememe.com>
2023-06-16 17:10:48 +0100

    bump

--- predict.py
+++ predict.py
@@ -28,31 +28,31 @@ class Predictor(BasePredictor):
         # output = self.style_transfer(processed_image, scale)
         return image
 
-    # def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0): 
-    #     """Return a sharpened version of the image, using an unsharp mask."""
-    #     blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-    #     sharpened = float(amount + 1) * image - float(amount) * blurred
-    #     sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
-    #     sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
-    #     sharpened = sharpened.round().astype(np.uint8)
-    #     if threshold > 0:
-    #         low_contrast_mask = np.absolute(image - blurred) < threshold
-    #         np.copyto(sharpened, image, where=low_contrast_mask)
+    def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0): 
+        """Return a sharpened version of the image, using an unsharp mask."""
+        blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+        sharpened = float(amount + 1) * image - float(amount) * blurred
+        sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+        sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+        sharpened = sharpened.round().astype(np.uint8)
+        if threshold > 0:
+            low_contrast_mask = np.absolute(image - blurred) < threshold
+            np.copyto(sharpened, image, where=low_contrast_mask)
         
-    #     return sharpened
+        return sharpened
 
-    # def style_transfer(content_img, style_image, style_weight = 1, content_weight = 1, style_blur=False):
-    #     content_img = unsharp_mask(content_img,amount=1)
-    #     content_img = tf.image.resize(tf.convert_to_tensor(content_img,tf.float32)[tf.newaxis,...] / 255.,(512,512),preserve_aspect_ratio=True)
-    #     style_img = tf.convert_to_tensor(style_image,tf.float32)[tf.newaxis,...] / 255.
-    #     if style_blur:
-    #         style_img=  tf.nn.avg_pool(style_img, [3,3], [1,1], "VALID")
-    #     style_img = tf.image.adjust_contrast(style_img, style_weight)        
-    #     content_img = tf.image.adjust_contrast(content_img,content_weight)     
-    #     content_img = tf.image.adjust_saturation(content_img, 2)        
-    #     content_img = tf.image.adjust_contrast(content_img,1.5)        
-    #     stylized_img = model(content_img, style_img)[0]
+    def style_transfer(content_img, style_image, style_weight = 1, content_weight = 1, style_blur=False):
+        content_img = unsharp_mask(content_img,amount=1)
+        content_img = tf.image.resize(tf.convert_to_tensor(content_img,tf.float32)[tf.newaxis,...] / 255.,(512,512),preserve_aspect_ratio=True)
+        style_img = tf.convert_to_tensor(style_image,tf.float32)[tf.newaxis,...] / 255.
+        if style_blur:
+            style_img=  tf.nn.avg_pool(style_img, [3,3], [1,1], "VALID")
+        style_img = tf.image.adjust_contrast(style_img, style_weight)        
+        content_img = tf.image.adjust_contrast(content_img,content_weight)     
+        content_img = tf.image.adjust_saturation(content_img, 2)        
+        content_img = tf.image.adjust_contrast(content_img,1.5)        
+        stylized_img = model(content_img, style_img)[0]
         
-    #     return Image.fromarray(np.uint8(stylized_img[0]*255))
+        return Image.fromarray(np.uint8(stylized_img[0]*255))
 
 
